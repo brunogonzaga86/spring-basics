ca9e933c44e39d39feef408b77168b3fb12d9639
Bruno Gonzaga Santos <brunog86@gmail.com>
2020-06-27 22:53:37 -0300

    thymeleaf

--- src/main/java/com/treehouse/gifhub/controller/GifController.java
+++ src/main/java/com/treehouse/gifhub/controller/GifController.java
@@ -2,21 +2,13 @@ package com.treehouse.gifhub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GifController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String listGifs() {
-        return "List of all gifs";
-    }
-
-    @RequestMapping("/gif")
-    @ResponseBody
-    public String returnGif() {
-        return "Hello World!";
+        return "home";
     }
 
 }
